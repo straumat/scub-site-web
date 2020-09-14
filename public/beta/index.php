fc05c05f3277f602ad4ccacf0fc463e032ce6f87
straumat <stephane.traumat@gmail.com>
2020-09-14 15:05:11 +0200

    Twig test

---
+++ public/beta/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// =====================================================================================================================
+// Load Twig.
+require __DIR__ . '/vendor/autoload.php';
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+// =====================================================================================================================
+// Run template Twig.
+echo $twig->render('first.html.twig');
